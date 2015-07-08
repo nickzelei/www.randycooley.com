2c23e23d04a82a2b339c82349dd7078678d5bdc0
Nick Zelei <nickzelei@gmail.com>
2015-07-07 21:50:34 -0500

    updates

--- public/js/controllers/AdminCtrl.js
+++
@@ -1,3 +0,0 @@
-angular.module('AdminCtrl', []).controller('AdminController', function($scope) {
-  $scope.tagline = 'This is the admin page.';
-})
---
+++ public/js/directives/projectMeta.js
@@ -0,0 +1,12 @@
+app.directive('projectMeta', function() {
+	return {
+		restrict: 'E',
+		scope: {
+			project: '='
+		},
+		replace: true,
+		templateUrl: "views/templates/projectMeta.html",
+		controller: function($scope) {
+		}
+	};
+});
