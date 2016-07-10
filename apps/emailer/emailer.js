92bd4b4f32cedda7f9cf20fea0d7ada5a0b6b214
Nick Zelei <nickzelei@gmail.com>
2016-07-10 14:59:13 -0500

    updated
    updates

--- apps/emailer/emailer.js
+++ apps/emailer/emailer.js
@@ -3,36 +3,36 @@
 const nodemailer = require('nodemailer');
 
 const emailConfig = {
-	'username': process.env.EMAIL_USERNAME,
-	'passwordToken': process.env.EMAIL_PASSWORDTOKEN,
+    'username': process.env.EMAIL_USERNAME,
+    'passwordToken': process.env.EMAIL_PASSWORDTOKEN,
     'sendTo': process.env.EMAIL_SENDTO
 };
 
 module.exports = {
-	sendMail: function(data, callback) {
-		const transporter = nodemailer.createTransport({
-			host: "smtp.gmail.com",
+    sendMail: function(data, callback) {
+        const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
             port: 465,
             secure: true,
-			auth: {
-		        user: emailConfig.username,
-		        pass: emailConfig.passwordToken
-	    }
-		});
-		console.log(emailConfig);
-		let subject = "New Inquiry from: " + data.name;
-		let text = "Name: " + data.name + "<br><br>" +
-					"Phone: " + data.phone + "<br><br>" + 
-					"Email: " + data.email + "<br><br>" + 
-					"Message: " + data.message;
+            auth: {
+                user: emailConfig.username,
+                pass: emailConfig.passwordToken
+            }
+          });
 
-		let message = {
-			from: emailConfig.username,
-			to: emailConfig.sendTo,
-			subject: subject,
-			html: text,
-			replyTo: data.email
-		};
+        const subject = "New Inquiry from: " + data.name;
+        const text = "Name: " + data.name + "<br><br>" +
+            "Phone: " + data.phone + "<br><br>" +
+            "Email: " + data.email + "<br><br>" +
+            "Message: " + data.message;
+
+        const message = {
+            from: emailConfig.username,
+            to: emailConfig.sendTo,
+            subject: subject,
+            html: text,
+            replyTo: data.email
+        };
 
 		transporter.sendMail(message, function(error, info) {
 			if (error) {
